bd9b07807f5eb90d3fc975b5c4bb7964939fe0e9
Sala <Salambaye2001@gmail.com>
2024-12-01 16:13:25 +0100

    User Interface App

--- app/src/main/java/com/example/userinterfaceapp/MainActivity.kt
+++ app/src/main/java/com/example/userinterfaceapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.userinterfaceapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -13,17 +15,47 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.userinterfaceapp.ui.theme.UserInterfaceAppTheme
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+
+        // Gestion du clic sur le bouton
         val btnNextActivity: Button = findViewById(R.id.btnNextActivity)
         btnNextActivity.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        // Gestion de la navigation dans le menu
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_main -> {
+                    Toast.makeText(this, "Vous êtes déjà sur la page principale", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_second -> {
+                    val intent = Intent(this, SecondActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_easter_egg -> {
+                    Toast.makeText(this, "Félicitations pour l'Easter Egg !", Toast.LENGTH_LONG).show()
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
+
+
     }
 
 
--- app/src/main/java/com/example/userinterfaceapp/SecondActivity.kt
+++ app/src/main/java/com/example/userinterfaceapp/SecondActivity.kt
@@ -2,10 +2,22 @@ package com.example.userinterfaceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val bookList = listOf(
+            Book("La maison du guet ", "May Higgins Clark"),
+            Book("7 ans après", "Guillaume Muso"),
+            Book("Vol de nuit", "Antoine de Saint-Exupéry")
+        )
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = BookAdapter(bookList)
     }
+
 }
